402b5c40bd29caf121086f65d41b9ffdaefbc7f2
naimaahsan <142057464+naimaahsan@users.noreply.github.com>
2026-07-14 15:53:56 +0100

    space model and repo class created and test files

---
+++ tests/test_space.py
@@ -0,0 +1,19 @@
+from lib.space import Space
+
+def test_instantiates():
+    space = Space("Cozy Cottage", "Guest Favourite", "Cornway", 100, 1, 1)
+    assert space.id == 1 
+    assert space.name == "Cozy Cottage"
+    assert space.address == "Cornway"
+    assert space.description == "Guest Favourite"
+    assert space.price_per_night == 100
+    assert space.user_id == 1
+
+def test_equality():
+    space_1 = Space("Cozy Cottage", "Guest Favourite", "Cornway", 100, 1, 1)
+    space_2 = Space("Cozy Cottage", "Guest Favourite", "Cornway", 100, 1, 1)
+    assert space_1 == space_2
+
+def test_formats_correctly():
+    space = Space("Cozy Cottage", "Guest Favourite", "Cornway", 100, 1, 1)
+    assert str(space) == "Space(1, Cozy Cottage, Guest Favourite, Cornway, 100, 1)"
