c27620e6a531669cfaf2645fb30efdf6bcfe641a
Georgina <147413772+georgie-hardcastle@users.noreply.github.com>
2026-07-17 10:15:49 +0100

    Fix indentation and formatting in app.py

--- app.py
+++ app.py
@@ -107,7 +107,9 @@ def get_my_booking():
     user_id = session['user_id']
 
     user_bookings = my_booking_repository.find_by_user_id(user_id)
-    return render_template("/mybookings.html", bookings=user_bookings, user_id=user_id)@app.route('/spaces/<int:id>', methods=["GET"])
+    return render_template("/mybookings.html", bookings=user_bookings, user_id=user_id)
+    
+@app.route('/spaces/<int:id>', methods=["GET"])
 @login_required
 def space_details(id):
     connection = get_flask_database_connection(app)
